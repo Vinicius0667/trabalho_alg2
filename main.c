52282585ebc3ccb10e9bc8a3dde8a921eaa0957c
Eridan <genealerooo@gmail.com>
2025-10-24 00:22:41 -0400

    Implementada o aumento do score
    Falta ainda excluir perguntas já feitas, ler corretamente perguntas do segundo bloco e utilizar os recursos.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "random_questions.h"
 // #include "embaralhar.h"
 
-void print_question(Question question, const unsigned short question_number, int recurso_plateia, int recurso_pular, int recurso_carta, int recurso_universitarios);
+void print_question(Question question, const unsigned short question_number, int recurso_plateia, int recurso_pular, int recurso_carta, int recurso_universitarios, double premio_total);
 
 int main(void) {
     Question *questions = NULL;
@@ -15,7 +15,7 @@ int main(void) {
     double premio_total = 0.0;
     char answer;
     int recurso_plateia = 3, recurso_pular = 3, recurso_carta = 3, recurso_universitarios = 3;
-    int nivel_dificuldade = 1, numeracao_pergunta = 1;
+    int nivel_dificuldade = 1;
     int loser = 0, winner = 0;
 
     file = fopen("perguntas.dat", "rb");
@@ -28,17 +28,13 @@ int main(void) {
     printf("\n------------------------------------------------------------------------------------------------------------------------------\n");
     printf("\n                                          SEJA BEM VINDO AO SHOW DO MILHÃO!!!                                                 \n");
     printf("\n------------------------------------------------------------------------------------------------------------------------------\n");
-
-    
-    // unsigned short question_number = 1;
-    // question_number = (question_number > 5) ? 1 : question_number;
     
     while(!loser && !winner && nivel_dificuldade <= 4){
         unsigned short questions_by_level = (nivel_dificuldade == 4) ? 10 : 20;
-
+        unsigned short acertos = 1;
+        
         unsigned short question_number = (nivel_dificuldade == 4) ? 1 : 5;
         unsigned short start_pos = (nivel_dificuldade - 1) * 20;
-        unsigned short nivel_atual = nivel_dificuldade;
 
         if (questions != NULL){
             free_questions(&questions);
@@ -46,49 +42,55 @@ int main(void) {
 
         alloc_questions(&questions, questions_by_level);
 
-        unsigned short count = 1;
-        while (nivel_atual == nivel_dificuldade ||  !loser){
+        if (nivel_dificuldade == 2) premio_total = 10000;
+        if (nivel_dificuldade == 3) premio_total = 100000;
+        // if(count == 1 && nivel_dificuldade == 4) premio_total = 1000000; GANHOU
+        
+        while (!loser){
             read_questions(&questions, file, start_pos, questions_by_level);
 
             unsigned short random_number = get_random_number(0,questions_by_level);
             Question *atual_question = &questions[random_number];
             
-            print_question(*atual_question, count, recurso_plateia, recurso_pular, recurso_carta, recurso_universitarios);
+            print_question(*atual_question, acertos, recurso_plateia, recurso_pular, recurso_carta, recurso_universitarios, premio_total);
             scanf(" %c", &answer);
 
             if (answer == atual_question->answer){
                 printf("\nVocê acertou!!!\n");
-                count += 1;
+                if(acertos >= 1 && nivel_dificuldade == 1) premio_total = 1000 * acertos;
+                if(acertos >= 1 && nivel_dificuldade == 2) premio_total = 10000 * (acertos +1);
+                if(acertos >= 1 && nivel_dificuldade == 3) premio_total = 100000 * (acertos +1);
+                acertos += 1;
             } else {
                 printf("\nVocê perdeu tudo!!! : (\n");
                 loser = 1;
                 break;
             }
 
-            if (count > question_number){
-                nivel_atual += 1;
+            if (acertos > question_number){
+                nivel_dificuldade++;
+                break;
             }
         }
 
-        nivel_dificuldade ++;
-        
+        if (loser) break;
     }
     
     fclose(file);
     return EXIT_SUCCESS;
 }
 
-void print_question(Question question, const unsigned short count, int recurso_plateia, int recurso_pular, int recurso_carta, int recurso_universitarios) {
+void print_question(Question question, const unsigned short acertos, int recurso_plateia, int recurso_pular, int recurso_carta, int recurso_universitarios, double premio_total) {
 
     
-    printf("\nPergunta %hu\n", count); // count vai ser aumentado na main, de 1 - 5
+    printf("\nPergunta %hu\n", acertos); // acertos vai ser aumentado na main, de 1 - 5
     printf("\n%s\n", question.question);
     
     printf("\na) %s\n", question.options[0]);
     printf("b) %s\n", question.options[1]);
     printf("c) %s\n", question.options[2]);
     printf("d) %s\n", question.options[3]);
-
+    // Perguntas do segundo Bloco não estão sendo lidas corretamentes...
     printf("\n------------------------------------------------------------------------------------------------------------------------------\n");
 
     printf("\n--- Ajuda ---\n");
@@ -99,7 +101,7 @@ void print_question(Question question, const unsigned short count, int recurso_p
     printf("[5] Parar\n");
     printf("\n------------------------------------------------------------------------------------------------------------------------------\n");
 
+    printf("\n== Voce possui um total acumulado de R$ %.2lf ==\n", premio_total);
     printf("Entre com a sua opção [a-d] para responder ou [1-5] para receber ajuda: ");
 }
 
-// printf("\n== Voce possui um total acumulado de R$ %.2lf ==\n", score);
